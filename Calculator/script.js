785f7d0b6ac1954b7d17222871e17a9d1ec87446
subhaharini04 <subhaharini.act@gmail.com>
2025-10-03 17:01:22 +0530

    completed operation

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,7 +3,7 @@ const buttons = document.querySelectorAll("button");
 let present = "";
 let number = [];
 let operator = [];
-
+let expression = "";
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         let btn = button.textContent;
@@ -11,6 +11,7 @@ buttons.forEach(button => {
             present = "";
             number = [];
             operator = [];
+            expression="";
             display.value = "";
         } else if (btn === "=") {
             number.push(parseFloat(present));
@@ -19,36 +20,40 @@ buttons.forEach(button => {
             present = result.toString();
             number = [];
             operator = [];
+            expression = result.toString();
         } else if (btn >= 0 && btn <= 9 || btn === ".") {
+             if (btn === "." && present.includes(".")) return;
             present += btn;
-            display.value = present;
+            expression += btn;
+            display.value = expression;
         } else {
             number.push(parseFloat(present));
             operator.push(btn);
             present = "";
-            display.value+=btn;
+            expression += btn;
+            display.value = expression;
 
         }
     });
 });
 
-function operation(num,op) {
-    for(let i=0;i<op.length;i++){
-        if(op[i]==="X"||op[i]==="÷"){
-            const result=operator[i]==="x" ? num[i]*num[i+1]:num[i]/num[i+1];
-            num.splice(i,2,result);
-            op.splice(i,1);
+function operation(num, op) {
+    for (let i = 0; i < op.length; i++) {
+        if (op[i] === "x" || op[i] === "÷") {
+            const result = operator[i] === "x" ? num[i] * num[i + 1] : num[i] / num[i + 1];
+            num.splice(i, 2, result);
+            op.splice(i, 1);
             i--;
         }
     }
-    let result=num[0];
-    for(let i=0;i<op.length;i++){
-        if(op[i]==="+"){
-            result+=num[i+1];
-        }else if(op[i]==="-"){
-            result-=num[i+1];
+    let result = num[0];
+    for (let i = 0; i < op.length; i++) {
+        if (op[i] === "+") {
+            result += num[i + 1];
+        } else if (op[i] === "-") {
+            result -= num[i + 1];
         }
     }
-        return result;
+    return result;
 
 }
