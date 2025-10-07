939390d52b4fe7ee1906dda4ec1cad150588a137
subhaharini04 <subhaharini.act@gmail.com>
2025-10-07 16:32:12 +0530

    Updated features

--- Todo/src/Task.java
+++ Todo/src/Task.java
@@ -3,13 +3,68 @@ import java.util.ArrayList;
 class setOfTask {
     String task;
     boolean isCompleted;
+    int priority;
 
-    setOfTask(String task) {
+    setOfTask(String task,int priority) {
         this.task = task;
         this.isCompleted = false;
+        this.priority=priority;
     }
 }
 
 public class Task{
     ArrayList<setOfTask> listOfTask=new ArrayList<>();
+
+    public void add(String tsk,int priority){
+        setOfTask task1=new setOfTask(tsk,priority);
+        listOfTask.add(task1);
+    }
+
+    public void view(){
+        listOfTask.sort((a, b) -> Integer.compare(a.priority, b.priority));
+        if(listOfTask.isEmpty()){
+            System.out.println("No Task Available!");
+        }
+        for (int i=0;i<listOfTask.size();i++){
+            setOfTask t=listOfTask.get(i);
+            String pText = switch (t.priority) {
+                case 1 -> "High";
+                case 2 -> "Medium";
+                case 3 -> "Low";
+                default -> "Unknown";
+            };
+            String status=t.isCompleted ? "[✓]":"[ ]";
+            System.out.println((i+1)+" "+status+" "+t.task+" "+ pText);
+        }
+    }
+    public void delete(int index){
+        if(index<=0 || index>listOfTask.size()){
+            System.out.println("Invalid task number");
+            return;
+        }
+        listOfTask.remove(index-1);
+        System.out.println("Deleted Successfully");
+        view();
+    }
+
+    public void complete(int index){
+        if(index<=0 || index>listOfTask.size()){
+            System.out.println("Invalid task number");
+            return;
+        }
+        listOfTask.get(index-1).isCompleted=true;
+        System.out.println("Task Completed Successfully");
+        view();
+    }
+
+    public void edit(int index,String newTask,int newPriority){
+        if(index<=0 || index>listOfTask.size()){
+            System.out.println("Invalid task number");
+            return;
+        }
+        listOfTask.get(index-1).task=newTask;
+        listOfTask.get(index-1).priority=newPriority;
+        System.out.println("Task Edited Successfully");
+        view();
+    }
 }
--- Todo/src/Todo.java
+++ Todo/src/Todo.java
@@ -5,28 +5,49 @@ public class Todo {
         Scanner obj=new Scanner(System.in);
         System.out.println("------Welcome to Todo------");
         boolean isRunning=true;
+        Task task=new Task();
         while (isRunning){
-            Task task=new Task();
             System.out.println("Operation: ");
-            System.out.println("1. Add Task\n2. View Task\n3. Delete Task\n4. Mark Task Completed\n5. Exit");
+            System.out.println("1. Add Task\n2. View Task\n3. Delete Task\n4. Mark Task Completed\n5. Edit Task\n6. Exit");
             System.out.print("Enter your Choice: ");
             int choice= obj.nextInt();
+            obj.nextLine();
             switch (choice){
                 case 1:
-                    System.out.println("Enter Task Name: ");
+                    System.out.print("Enter task Name: ");
                     String tsk= obj.nextLine();
-                    task.add();
+                    System.out.println("Priority:\n1. High\n2. Mid\n3. Low");
+                    System.out.print("Enter priority: ");
+                    int priority=obj.nextInt();
+                    obj.nextLine();
+                    task.add(tsk,priority);
                     break;
                 case 2:
                     task.view();
                     break;
                 case 3:
-                    task.delete();
+                    System.out.print("Enter task number to delete: ");
+                    int index= obj.nextInt();
+                    task.delete(index);
                     break;
                 case 4:
-                    task.complete();
+                    System.out.print("Enter task number to Complete: ");
+                    int index1= obj.nextInt();
+                    task.complete(index1);
                     break;
                 case 5:
+                    System.out.print("Enter task number to Edit: ");
+                    int index3= obj.nextInt();
+                    obj.nextLine();
+                    System.out.print("Enter new task name: ");
+                    String newTask= obj.nextLine();
+                    System.out.println("Priority:\n1. High\n2. Mid\n3. Low");
+                    System.out.print("Enter priority: ");
+                    int priority1=obj.nextInt();
+                    obj.nextLine();
+                    task.edit(index3,newTask,priority1);
+                    break;
+                case 6:
                     isRunning=false;
                     System.out.println("Thank You");
                     break;
