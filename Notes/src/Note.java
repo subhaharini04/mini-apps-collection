80fb1a5a4883123901fc92aa499e36677dd60c32
subhaharini04 <subhaharini.act@gmail.com>
2025-10-08 13:49:49 +0530

    Complete Note app

--- Notes/src/Note.java
+++ Notes/src/Note.java
@@ -1,2 +1,111 @@
-public class Note {
+import java.io.*;
+import java.util.ArrayList;
+
+class setOfNote {
+  String title;
+  String content;
+
+    public setOfNote(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String toString(){
+        return title + " : "+ content;
+    }
+}
+
+public class Note{
+    public void add(String title,String content){
+        if(title.isEmpty()|| content.isEmpty()){
+            System.out.println("Title or content cannot be empty");
+            return;
+        }
+        try(BufferedWriter writer=new BufferedWriter(new FileWriter("Notes.txt",true))){
+            writer.write(title+" | "+content);
+            writer.newLine();
+        }catch (IOException e){
+            System.out.println("Error in adding");
+            e.printStackTrace();
+        }
+        System.out.println("Added");
+        view();
+    }
+
+    public void view(){
+        try(BufferedReader reader=new BufferedReader(new FileReader("Notes.txt"))) {
+           String line;
+           boolean found=false;
+           int count=1;
+           while((line=reader.readLine())!=null){
+               found=true;
+               System.out.println(count+". "+line);
+           }
+           if(!found) System.out.println("No notes found");
+        }catch (IOException e){
+            System.out.println("Error in Showing Notes");
+        }
+    }
+
+    public void delete(int index){
+        ArrayList<String> list=new ArrayList<>();
+       try(BufferedReader reader=new BufferedReader(new FileReader("Notes.txt"))) {
+           String line;
+           while ((line=reader.readLine())!=null){
+               list.add(line);
+           }
+       }catch (IOException e){
+           e.printStackTrace();
+       }
+        if (list.isEmpty()) {
+            System.out.println("No notes to delete");
+            return;
+        }
+        if(index<=0 || index>list.size()) {
+            System.out.println("Index is invalid");
+            return;
+        }
+        list.remove(index-1);
+       try(BufferedWriter writer=new BufferedWriter(new FileWriter("Notes.txt",false))) {
+         for(int i=0;i< list.size();i++){
+             writer.write(list.get(i));
+             writer.newLine();
+         }
+       }catch (IOException e){
+           e.printStackTrace();
+       }
+       view();
+    }
+
+    public void edit(int index,String note,String content){
+        ArrayList<String> list=new ArrayList<>();
+        try(BufferedReader reader=new BufferedReader(new FileReader("Notes.txt"))) {
+            String line;
+            while ((line=reader.readLine())!=null){
+                list.add(line);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        if (list.isEmpty()) {
+            System.out.println("No notes to edit");
+            return;
+        }
+
+        if(index<=0 || index> list.size()) {
+            System.out.println("Index is invalid");
+            return;
+        }
+        list.set(index-1,note+" | "+content);
+        try(BufferedWriter writer=new BufferedWriter(new FileWriter("Notes.txt",false))) {
+            for(int i=0;i< list.size();i++){
+                writer.write(list.get(i));
+                writer.newLine();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        view();
+    }
+
 }
--- Notes/src/NotesApp.java
+++ Notes/src/NotesApp.java
@@ -1,2 +1,51 @@
+import java.util.Scanner;
+
 public class NotesApp {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("------Welcome to Note App------");
+        boolean isRunning=true;
+        Note note=new Note();
+        while (isRunning){
+            System.out.println("Operation: ");
+            System.out.println("1. Add Note\n2. View Notes\n3. Delete Note\n4. Edit Note\n5. Exit");
+            System.out.print("Enter your Choice: ");
+            int choice= obj.nextInt();
+            obj.nextLine();
+            switch (choice){
+                case 1:
+                    System.out.print("Enter Title: ");
+                    String title= obj.nextLine();
+                    System.out.print("Enter Content: ");
+                    String content=obj.nextLine();
+                    note.add(title,content);
+                    break;
+                case 2:
+                    note.view();
+                    break;
+                case 3:
+                    System.out.print("Enter Note number to delete: ");
+                    int index= obj.nextInt();
+                    note.delete(index);
+                    break;
+                case 4:
+                    System.out.print("Enter Note number to Edit: ");
+                    int index2= obj.nextInt();
+                    obj.nextLine();
+                    System.out.print("Enter new Title: ");
+                    String newNote= obj.nextLine();
+                    System.out.print("Enter new Content: ");
+                    String newContent= obj.nextLine();
+                    note.edit(index2,newNote,newContent);
+                    break;
+                case 5:
+                    isRunning=false;
+                    System.out.println("Thank You");
+                    break;
+                default:
+                    System.out.println("Enter correct choice");
+                    break;
+            }
+        }
+    }
 }
